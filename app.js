6b17b2dedf11e328034bf574dd6ce3847e4e4fa0
ethanmmay <ethaniel.may@gmail.com>
2021-02-15 11:16:51 -0700

    Constructor Practice

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-let car = {
-    
+let corolla = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2004,
+    price: 3000,
+    color: "Bronze"
 }
+
+let forester = {
+    make: "Subaru",
+    model: "Forester",
+    year: 2017,
+    price: 27000
+}
+
+let cars = [corolla, forester]
+
+class Car {
+    constructor (make, model, year, price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+}
+
+cars = [new Car("Toyota", "Corolla", 2004, 3000), new Car("Subaru", "Forester", 2017, 27000)]
