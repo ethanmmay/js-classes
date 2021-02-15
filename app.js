2f3707cf194dc6a66d5f3efcd218d7f29471cd59
ethanmmay <ethaniel.may@gmail.com>
2021-02-15 11:57:46 -0700

    Horns & forEach

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let corolla = {
     model: "Corolla",
     year: 2004,
     price: 3000,
+    horn: "honk honk",
     color: "Bronze"
 }
 
@@ -10,18 +11,23 @@ let forester = {
     make: "Subaru",
     model: "Forester",
     year: 2017,
-    price: 27000
+    price: 27000,
+    horn: "beep beep"
 }
 
-let cars = [corolla, forester]
-
 class Car {
-    constructor (make, model, year, price) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-        this.price = price;
+    constructor (make, model, year, price, horn, color) {
+        this.make = make
+        this.model = model
+        this.year = year
+        this.price = price
+        this.horn = horn
+        this.color = color || undefined
     }
 }
 
-cars = [new Car("Toyota", "Corolla", 2004, 3000), new Car("Subaru", "Forester", 2017, 27000)]
+let cars = [new Car(forester.make, forester.model, forester.year, forester.price, forester.horn), new Car(corolla.make, corolla.model, corolla.year, corolla.price, corolla.horn, corolla.color)]
+let cars2 = [new Car("Ford", "F-150", 2004, 7000, "HONK", "white"), new Car("Audi", undefined, 2016, 35000, "beeeep", "silver")]
+
+cars.forEach(car => console.log(car.horn))
+cars2.forEach(car => console.log(car.horn))
